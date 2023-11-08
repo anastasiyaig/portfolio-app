a3fe7007d9e5df7e0a20c9743f5161021ee6c0bb
Anastasiya Semenkevich <anastasija.ig@gmail.com>
2023-11-08 10:03:43 +0300

    Adjust description

--- main.py
+++ main.py
@@ -9,9 +9,16 @@ with column1:
 
 with (column2):
     st.title("Anastasiya S")
-    content = """
-    Hi, I am Anastasiya and everyone calls me Nastya
-    """
+    content = """Hi, I am Anastasiya and everyone calls me Nastya. I am open-minded and very friendly team player, 
+    enjoying making a process of software development easier and understandable for all the participants. Bringing 
+    awareness of what and why QA are doing on the project, enthusiastic to establish testing processes from scratch 
+    and mentoring newcomers. Truly believe in Quality Assurance (not "testing" :) ), very communicative and straight 
+    forward person, open to any opportunity to share knowledge and gain some where possible. I do have a various 
+    experience of QA with mobile, desktop and web applications for more than 10 years in total now. I am good in 
+    manual testing, variety of non-testing activities such as clarifying requirements, designs, talking to people and 
+    making things clear, as well as in automation with Python. Interested in finance and banking industry, blockchain 
+    technology and crypto adoption and my true passion is vehicles and related software (hope to get a dream job at 
+    Honda some day :))"""
     st.info(content)
 
 content_more = \
