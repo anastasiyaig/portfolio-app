6dd3154609306a89ae5bf85f4cfcd031f32cf5d8
Anastasiya Semenkevich <anastasija.ig@gmail.com>
2023-11-08 10:35:57 +0300

    Render the titles of the columns

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout="wide")
 
 column1, column2 = st.columns(2)
 
 with column1:
-    st.image("images/camphoto.jpeg", width=600)
+    st.image("images/camphoto.jpeg", width=300)
 
 with (column2):
     st.title("Anastasiya S")
@@ -26,4 +27,17 @@ content_more = \
 Below you can find some of the apps I have built in Python. Feel free to contact me!
     """
 
-st.write(content_more)
+st.caption(content_more)
+
+
+column3, column4 = st.columns(2)
+
+dataframe = pandas.read_csv("data.csv", sep=";")
+
+with column3:
+    for index, row in dataframe[:10].iterrows():
+        st.header(row["title"])
+
+with column4:
+    for index, row in dataframe[10:].iterrows():
+        st.header(row["title"])
